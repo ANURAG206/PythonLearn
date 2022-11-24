f48c14329b2201f93e455878820ecb56425d62cb
anuragdhull <dhullanuag206@gmail.com>
2022-11-24 19:58:11 +0530

    class 1

---
+++ class1.py
@@ -0,0 +1,11 @@
+#print("Hello world")
+#print("subscribe code with harry")
+#remove data remove data
+""" this is comment"""
+#print("anything op",end="")
+#print("hello")
+#print("hello op opop opo ","opopopopop")
+#print("c:\\harry")
+#print("c:\"op")
+
+print("hello boys\n and girls")
